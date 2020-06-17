de58984c240ac8eb60cece275427007a2794421d
mthoma <mthoma@mthoma-VirtualBox>
2020-06-17 07:37:40 +0200

    style sheets, login_page and LoginController continued.

--- demo-web-application/src/main/java/de/mthoma/demowebapp/controller/LoginController.java
+++ demo-web-application/src/main/java/de/mthoma/demowebapp/controller/LoginController.java
@@ -8,8 +8,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginController {
 
 
+	/**
+	 * 
+	 * @param model
+	 * @return
+	 */
 	@GetMapping(value = "/login")
 	public String login(Model model) {
+		
+		model.addAttribute("login_error_message_cls", "cls_invisible");
+        
+        return "login_page";
+	}
+	
+	/**
+	 * 
+	 * @param model
+	 * @return
+	 */
+	@GetMapping(value = "/login?error=true")
+	public String loginOnError(Model model) {
+		
+		model.addAttribute("login_error_message_cls", "cls_visible");
         
         return "login_page";
 	}
